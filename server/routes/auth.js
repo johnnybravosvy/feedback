a1549eb136d463fd926e0fa9fa29dbb4c6688066
Chibuzor Ezeufoh <Ezeufohchibuzor@gmail.com>
2024-05-10 01:02:15 +0100

    refactoring the admin model

---
+++ server/routes/auth.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { Admin } from "../models/Admin";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const router = express.Router();
+
+Router.post("/login", async (req, res) => {
+  const { username, password, role } = req.body;
+  if (role === "admin") {
+    const admin = await Admin.findOne({ username });
+    if (!admin) {
+      return res.json({ message: "admin not registered" });
+    }
+    const validPassword = await bcrypt.compare(password, admin.password);
+    if (!validPassword) {
+      return res.json({ message: "wrong password" });
+    }
+    const token = jwt.sign(
+      { username: admin.username, role: "admin" },
+      process.env.Admin_key
+    );
+  } else if (role === "student") {
+  }
+});
